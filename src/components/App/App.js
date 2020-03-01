1c9388b78491cddd912f2a1889ec801bf1aa21b6
Derek Boat <derek.boat@gmail.com>
2020-03-01 11:21:36 -0600

    setting up first route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import { connect } from 'react-redux';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import Feeling from '../Feeling/Feeling';
+
+
 
 class App extends Component {
+
+
+  componentDidMount = () => {
+    console.log('component mounted');
+  }
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Feedback!</h1>
+            <h4><i>Don't forget it!</i></h4>
+          </header>
+          <br />
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+const putReduxStateOnProps = (reduxState) => ({
+  reduxState
+})
+
+export default connect(putReduxStateOnProps)(App);
